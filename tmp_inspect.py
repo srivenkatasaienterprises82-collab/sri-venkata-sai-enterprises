82493b8be5b4dc979db10d08ee2424993cbe1e43
brkpod-oss <brkpod@gmail.com>
2026-07-11 19:17:14 +0530

    Remove accidentally committed helper/temp scripts

--- tmp_inspect.py
+++
@@ -1,6 +0,0 @@
-import sys
-with open('automation/launch_checker.py', 'r', encoding='utf-8') as f:
-    lines = f.readlines()
-    for i in range(37, 116):
-        prefix = lines[i][:8].encode('utf-8').hex()
-        print(f"{i+1:3d} +{prefix}+ {repr(lines[i][:120])}")
